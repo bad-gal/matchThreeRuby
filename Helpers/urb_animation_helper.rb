678d1297958e8934713886463a10b64da43266b6
Andrien Ricketts <andrien26@gmail.com>
2018-04-20 09:19:01 +0100

    Minor changes
    Use methods from Game_Module
    Add method urb_file_type extracted from another Helper class
    Create empty methods that will hold the instructions for the
    various sweet treat options

--- Helpers/urb_animation_helper.rb
+++ Helpers/urb_animation_helper.rb
@@ -1,3 +1,5 @@
+require_relative 'game_module'
+
 module UrbAnimationHelper
   def self.change(object, num)
     case num
@@ -101,6 +103,36 @@ module UrbAnimationHelper
     [filename, w, h]
   end
 
+  def self.urb_file_type(number)
+    case number
+    when 0
+      file = 'assets/rocker_anim.png'
+      type = :rocker
+    when 1
+      file = 'assets/pac_anim.png'
+      type = :pac
+    when 2
+      type = :pigtails
+      file = 'assets/pigtails_anim.png'
+    when 3
+      type = :punk
+      file = 'assets/punk_anim.png'
+    when 4
+      type = :nerd
+      file = 'assets/nerd_anim.png'
+    when 5
+      type = :nerd_girl
+      file = 'assets/nerd_girl_anim.png'
+    when 6
+      type = :baby
+      file = 'assets/baby_anim.png'
+    when 7
+      type = :lady
+      file = 'assets/lady_anim.png'
+    end
+    { file: file, type: type }
+  end
+
   def self.sweet_transformation(object)
     case object.type
     when :MINT_SWEET
@@ -113,5 +145,137 @@ module UrbAnimationHelper
       filename = 'assets/cookie.png'
     end
     object.change_image(filename)
+  end
+  
+  def self.sweet_fade(object)
+    case object.type
+    when :MINT_SWEET
+      filename = 'assets/mint_sweet_fade.png'
+    when :PURPLE_SWEET
+      filename = 'assets/purple_sweet_fade.png'
+    when :GOBSTOPPER
+      filename = 'assets/gobstopper_fade.png'
+    when :COOKIE
+      filename = 'assets/cookie_fade.png'
+    end
+    object.add_frame_data(5, Settings::BOUNCE_DURATION, Settings::BOUNCE_FPS)
+    object.change_image_and_loop(filename, false)
+  end
+  
+  #------------------------------------------------------
+  # assign the right method depending on the sweet treats
+  #------------------------------------------------------
+  def self.special_treat(object1, object2, objects, width, height, obstacles)
+    p "This is a special treat"
+    result = []
+    arr = [object1, object2]
+
+    arr.each do |ar|
+      result << ar if Settings::SWEET_TREATS.include?(ar.type)
+    end
+
+    if result.size == 1
+      case result.first.type
+      when :MINT_SWEET
+        sfx = horizontal_effects(result.first)
+        return [basic_stripe_sweet(result.first, objects, width, obstacles), sfx, [result.first]]
+      when :PURPLE_SWEET
+        sfx = vertical_effects(result.first)
+        return [basic_stripe_sweet(result.first, objects, width, obstacles), sfx, [result.first]]
+      when :COOKIE
+        basic_cookie(result.first)
+      when :GOBSTOPPER
+        basic_gobstopper(result.first)
+      end
+    else # double treats
+      if result.first.type == result.last.type
+        case result.first.type
+        when :MINT_SWEET, :PURPLE_SWEET
+          double_stripe_sweet(result.first, result.last)
+        when :COOKIE
+          double_cookie(result.first, result.last)
+        when :GOBSTOPPER
+          double_gobstopper(result.first, result.last)
+        end
+      elsif [result.first.type, result.last.type].all? { |o| [:MINT_SWEET, :PURPLE_SWEET] }
+        double_stripe_sweet(result.first, result.last)
+      elsif [result.first.type, result.last.type].all? { |o| [:MINT_SWEET, :COOKIE] }
+        sweet_cookie(result.first, result.last)
+      elsif [result.first.type, result.last.type].all? { |o| [:MINT_SWEET, :GOBSTOPPER] }
+        sweet_gobstopper(result.first, result.last)
+      elsif [result.first.type, result.last.type].all? { |o| [:PURPLE_SWEET, :COOKIE] }
+        sweet_cookie(result.first, result.last)
+      elsif [result.first.type, result.last.type].all? { |o| [:PURPLE_SWEET, :GOBSTOPPER] }
+        sweet_gobstopper(result.first, result.last)
+      elsif [result.first.type, result.last.type].all? { |o| [:COOKIE, :GOBSTOPPER] }
+        cookie_gobstopper(result.first, result.last)
+      end
+    end
+  end
+  
+  #----------------------------------------------------------
+  # identify the objects that need to bounce out [be removed]
+  #----------------------------------------------------------
+  def self.basic_stripe_sweet(object, objects, width, obstacles)
+    if object.type == :MINT_SWEET # horizontal
+      loc = object.location / width
+      matches = objects.find_all { |o| o.location / width == loc && o.location != object.location }.map { |o| o.location }
+    else # vertical
+      loc = object.location % width
+      matches = objects.find_all { |o| o.location % width == loc && o.location != object.location }.map { |o| o.location }
+    end
+    matched_details = [{ matches: matches, shape: :LINE, intersects: nil, special_type: nil }]
+    GameModule.obstacle_contained_in_match(obstacles, matched_details)
+    matched_details
+  end
+
+  #----------------------------------------------------------
+  # estimate the purple sweet special effects
+  #----------------------------------------------------------
+  def self.vertical_effects(object)
+    x = object.x
+    y = object.y + 31
+    fps = Settings::FPS
+    duration = 5000
+    looped = false
+    sweet_fade(object)
+    [SpecialFX.new('assets/lightning2r.png', x, y, fps, duration, 2, object.type)]
+  end
+
+  #----------------------------------------------------------
+  # estimate the mint sweet special effects
+  #----------------------------------------------------------
+  def self.horizontal_effects(object)
+    x = object.x + 21
+    y = object.y + 42
+    fps = Settings::FPS
+    duration = 5000
+    looped = false
+    sweet_fade(object)
+    [SpecialFX.new('assets/lightning2r.png', x, y, fps, duration, 2, object.type)]
+  end
+
+  def self.double_stripe_sweet(object1, object2)
+  end
+
+  def self.double_cookie(object1, object2)
+  end
+
+  def self.double_gobstopper(object1, object2)
+  end
+
+  def self.basic_cookie(object)
+  end
+
+  def self.basic_gobstopper(object)
+  end
+
+  def self.sweet_cookie(object1, object2)
+  end
+
+  def self.sweet_gobstopper(object1, object2)
+  end
+
+  def self.cookie_gobstopper(object1, object2)
   end
 end
